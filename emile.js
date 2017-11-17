14d9b841872ecb7eff3b3166ceb7d370039b32b5
Gauthier <Hinosxz@MacBook-Pro-de-Gauthier.local>
2017-11-17 16:17:09 +0100

    Simple 2-player game with rooms implementation

--- emile.js
+++ emile.js
@@ -1,13 +1,19 @@
 var io;
 var gameSocket;
-var gameId = 'first room';
 var playersIds = [];
 var players = [];
 
 class Player{
-  constructor(id, value){
+  constructor(id, name, gameId){
     this.id = id;
-    this.value = value;
+    this.name = name;
+    this.game = gameId;
+    this.value = 0;
+  }
+
+  roll(){
+    this.value = getRandomIntInclusive(1,6);
+    return this.value;
   }
 }
 
@@ -21,10 +27,11 @@ exports.initGame = function(sio, socket){
     io = sio;
     gameSocket = socket;
     gameSocket.emit('connected', { message: "You are connected!" });
-    gameSocket.join(gameId);
 
     // Player Events
+    gameSocket.on('playerStart', playerStart);
     gameSocket.on('playerRoll', playerRoll);
+    gameSocket.on('playerWantsResults', playerShowResults);
 }
 
 //Generating a random integer
@@ -34,30 +41,64 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function playerRoll(){
-  if (players.length < 2){
-    if (!playersIds.includes(this.id)){
-      playersIds.push(this.id);
-      var player = new Player(this.id, getRandomIntInclusive(1,6));
-      players.push(player);
-      io.sockets.in(gameId).emit('playerRolled', player);
-      if (players.length == 2){
-        if (players[0].value < players[1].value){
-          io.sockets.in(gameId).emit('duel', players[1].id);
-        }
-        else if (players[0].value > players[1].value){
-          io.sockets.in(gameId).emit('duel', players[0].id);
-        }
-        else{
-          io.sockets.in(gameId).emit('duel', -1);
-        }
-      }
-    }
-    else{
-      io.sockets.in(gameId).emit('playerAlreadyRolled');
-    }
+function playerStart(data){
+  console.log('Player ' + data.playerName + ' attempting to join game: ' + data.gameId );
+
+  var player = new Player(this.id, data.playerName, data.gameId);
+
+  // Join the room
+  this.join(data.gameId);
+
+  // Look up the room ID in the Socket.IO manager object.
+  var room = io.nsps['/'].adapter.rooms[data.gameId];
+  if (!room.players){
+    room.players = [];
   }
-  else {
-    this.emit('gameOver');
+  room.players.push(player);
+  console.log(room.players);
+
+  console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
+
+  // Emit an event notifying the clients that the player has joined the room.
+  io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
+
+  if (room.length == 2){
+    io.sockets.in(data.gameId).emit('roomIsFull');
+    console.log('Room is full!');
   }
+  else{
+    console.log('Room is not full!');
+  }
+
+}
+
+function getPlayerPosition(gameId, id){
+  var room = io.nsps['/'].adapter.rooms[gameId];
+  if (room.players[0].id === id){
+    return 0;
+  }
+  else{
+    return 1;
+  }
+}
+
+function playerRoll(data){
+  var playerPosition = getPlayerPosition(data.gameId, this.id);
+  var room = io.nsps['/'].adapter.rooms[data.gameId];
+  data.value = room.players[playerPosition].roll();
+  io.sockets.in(data.gameId).emit('playerRolled', data);
+}
+
+function playerShowResults(data){
+  var room = io.nsps['/'].adapter.rooms[data.gameId];
+  if (room.players[0].value == room.players[1].value){
+    data.draw = true;
+  }
+  else if (room.players[0].value < room.players[1].value){
+    data.winnerName = room.players[1].name;
+  }
+  else{
+    data.winnerName = room.players[0].name;
+  }
+  this.emit('showResults', data);
 }
--- public/game.js
+++ public/game.js
@@ -1,38 +1,62 @@
 // Make connection
 var socket = io.connect('http://localhost:4000');
 
+//Query DOM
+var resultsWindow,
+    $doc = $(document);
+
+var gameId;
+var playerName;
+
 socket.on('connected', function (data){
   console.log(data.message);
+  // Shows the subscription page
+  $("#display-screen").html($("#join-game-template").html());
 });
+
+$doc.on('click', '#btnStart', function(){
+  console.log('I clicked start!');
+  data = {gameId: $('#inputGameId').val(), playerName: $('#inputPlayerName').val()};
+  gameId = data.gameId;
+  playerName = data.playerName;
+  socket.emit('playerStart', data);
+});
+
+socket.on('playerJoinedRoom', function(data){
+  //do something to the client side page
+  console.log(data.playerName + ' joined the room');
+});
+
+socket.on('roomIsFull', function(){
+  //starts the game
+  console.log('Room is full!');
+  $("#display-screen").html($("#play-game-template").html());
+  resultsWindow = $("#results")[0];
+});
+
+$doc.on('click', '#btnRoll', function(){
+  console.log('I clicked roll!');
+  var data = {gameId: gameId, playerName: playerName};
+  console.log(data);
+  socket.emit('playerRoll', data);
+});
+
+$doc.on('click', '#btnGetResults', function(){
+  console.log('I want results!');
+  var data = {gameId: gameId, playerName: playerName};
+  console.log(data);
+  socket.emit('playerWantsResults', data);
+});
+
 socket.on('playerRolled', function (data){
-  resultsWindow.innerHTML += '<p> Player n°' + data.id + ' got a <strong>' + data.value + '</strong>!</p>'
+  resultsWindow.innerHTML += '<p> Player ' + data.playerName + ' got a <strong>' + data.value + '</strong>!</p>'
 });
 
-//Query DOM
-var rollButton = $('#roll-button')[0],
-    resultsWindow = $('#results')[0];
-
-//Emit events
-rollButton.addEventListener('click', function(){
-  socket.emit('playerRoll');
-});
-
-
-//Listen to events
-
-socket.on('duel', function(winnerId){
-  if (winnerId == -1) {
+socket.on('showResults', function(data){
+  if (data.draw) {
     resultsWindow.innerHTML += "<p>It's a draw!</p>"
   }
   else {
-    resultsWindow.innerHTML += '<p> Player n°' + winnerId + ' won!</p>'
+    resultsWindow.innerHTML += '<p> Player ' + data.winnerName + ' won!</p>'
   }
-});
-
-socket.on('playerAlreadyRolled', function(){
-  alert('You already rolled the dice!');
-});
-
-socket.on('gameOver', function(){
-  alert('The game is over!');
 });
